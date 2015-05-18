040820b9a20190b920d826595dcc3d3a367d3f11
Cameron Webster <cameron.webster@gmail.com>
2015-05-18 09:54:57 -0600

    iteration zero all done

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	console.log("The document is ready!");
+
+	$('.light').on('click', function(){
+		$('body').css("background","url('img/tileable_wood_texture.png') repeat" );
+	});
+	$('.dark').on('click', function(){
+		$('body').css("background","url('img/dark_wood.png') repeat" );
+	});
+	$('.forest').on('click', function(){
+		$('body').css({"background": "url('img/forest.jpg')", "background-size": "cover"});
+	});
 })
